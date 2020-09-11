19fd0695ab2e7e7ac82686bf9f0ef18586e7c8f5
Tim <TimCPB@github.com>
2020-09-11 17:35:21 +0100

    add Squares as state in parent Board component

--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ constructor(props) {
 
   render() {
     return (
-      <button className="square" onClick={() => { alert('click'); }}>
+      <button
+        className="square"
+        onClick={() => { this.setState({value: 'X'}); }}
+      >
         {this.props.value}
       </button>
     );
@@ -19,8 +22,20 @@ constructor(props) {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() =>this.handleClick(i)}
+      />
+    );
   }
 
   render() {
